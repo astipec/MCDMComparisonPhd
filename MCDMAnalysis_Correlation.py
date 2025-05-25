9e085672aa310ad3e02ef975072b3206b577ce11
antons <antonstipec@gmail.com>
2025-05-25 14:01:52 +0200

    Added code for drawing plots for correlations

--- MCDMAnalysis_Correlation.py
+++ MCDMAnalysis_Correlation.py
@@ -36,6 +36,48 @@ def Create_Heatmap(DataFrameMatrix: pd.DataFrame, Method, Path, name, Cmap):
     print("Created heatmap: " + Method+"_Heatmap for the " + name)
     print('-' * 58)
 
+def Create_ScatterPlots(Kendall_df: pd.DataFrame, Spearman_df: pd.DataFrame, Pearson_df: pd.DataFrame, directory):
+    kendall = Kendall_df.loc['DEX'].drop('DEX')
+    spearman = Spearman_df.loc['DEX'].drop('DEX')
+    pearson = Pearson_df.loc['DEX'].drop('DEX')
+
+    df = pd.DataFrame({
+        "Kendall's Tau": kendall,
+        "Spearman's Rho": spearman,
+        "Pearson's R": pearson,
+        "Method": kendall.index
+    })
+
+    # Pairwise scatter plot of correlation metrics
+    sns.pairplot(df, vars=["Kendall's Tau", "Spearman's Rho", "Pearson's R"], diag_kind="auto")
+    plt.suptitle("Pairwise Scatter Plot of Correlations", y=1.02)
+    plt.savefig(directory + "/Pairwise_Scatter_DEX_Methods.png")
+    plt.clf()
+    plt.close()
+
+    # Line plot
+    plt.plot(["AHP", "TOPSIS", "PROMETHEE", "PAPRIKA"], df["Kendall's Tau"], label="Kendall's Tau")
+    plt.plot(["AHP", "TOPSIS", "PROMETHEE", "PAPRIKA"], df["Spearman's Rho"], label="Spearman's Rho")
+    plt.plot(["AHP", "TOPSIS", "PROMETHEE", "PAPRIKA"], df["Pearson's R"], label="Pearson's R")
+    plt.legend()
+    plt.title("Comparison of Correlation Metrics between DEX and other methods")
+    plt.savefig(directory + "/Comparison_DEX_Methods.png")
+    plt.clf()
+    plt.close()
+
+    # Bar plot for correlations
+    df.plot(kind="bar", figsize=(10, 6), alpha=0.8)
+    plt.title("Correlation metrics between DEX and MCDM methods")
+    plt.ylabel("Correlation Coefficient")
+    plt.xlabel("MCDM methods")
+    plt.axhline(0, color="gray", linestyle="--", linewidth=0.8)  # Reference line for zero
+    plt.legend(title="Metrics", bbox_to_anchor=(1.05, 1), loc="upper left")
+    plt.tight_layout()
+    plt.savefig(directory + "/Bar_Plot_DEX_Methods.png")
+    plt.clf()
+    plt.close()
+
+
 directory = './Results'
 if not os.path.exists(directory):
     os.makedirs(directory)
@@ -81,3 +123,6 @@ Kendall_df = CalculateCorrelation(MCDMRankings_df, "kendall", directory, "Kendal
 Create_Heatmap(Pearson_df, "Pearson", directory, "Pearson_MCDMRankings", "coolwarm")
 Create_Heatmap(Spearman_df, "Spearman", directory, "Spearman_MCDMRankings", "coolwarm")
 Create_Heatmap(Kendall_df, "Kendall", directory, "Kendall_MCDMRankings", "coolwarm")
+
+#Create scatter line plot to display correlation metrics between DEX and other MCDM methods
+Create_ScatterPlots(Kendall_df, Spearman_df, Pearson_df, directory)
